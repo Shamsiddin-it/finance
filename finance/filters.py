5916dd2baa5eec19d2eb61d47422ff1771c85127
Shamsiddin-it <samsiddinarbobov@gmail.com>
2024-11-27 16:18:18 +0500

    second

--- finance/filters.py
+++ finance/filters.py
@@ -6,14 +6,20 @@ class IncomeFilter(django_filters.FilterSet):
     start_date = django_filters.DateFilter(field_name="time", lookup_expr='gte', label='Start Date')
     end_date = django_filters.DateFilter(field_name="time", lookup_expr='lte', label='End Date')
 
+    month = django_filters.NumberFilter(field_name="time__month", lookup_expr='exact', label="Month")
+    year = django_filters.NumberFilter(field_name="time__year", lookup_expr='exact', label="Year")
+
     class Meta:
         model = Income
-        fields = ['start_date', 'end_date']
+        fields = ['start_date', 'end_date', 'month', 'year']
 
 class ExpenceFilter(django_filters.FilterSet):
     start_date = django_filters.DateFilter(field_name="time", lookup_expr='gte', label='Start Date')
     end_date = django_filters.DateFilter(field_name="time", lookup_expr='lte', label='End Date')
 
+    month = django_filters.NumberFilter(field_name="time__month", lookup_expr='exact', label="Month")
+    year = django_filters.NumberFilter(field_name="time__year", lookup_expr='exact', label="Year")
+
     class Meta:
         model = Expence
-        fields = ['start_date', 'end_date']
+        fields = ['start_date', 'end_date', 'month', 'year']
